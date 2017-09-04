3afdf376948a15272132d186a0a04cc342820083
Ahmad Waheed <ammwaheed@ucdavis.edu>
2017-09-03 18:44:24 -0700

    Style Nav.js

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,16 +4,46 @@ var React = require('react');
 //The syntaax will create the var. Link and use the "".Link" from router
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-  return (
-  <div>
-    <h2>Nav Component</h2>
+var Nav = React.createClass ({
+    onSearch: function(e) {
+      e.preventDefault();
+      alert('Not yet wired up');
+    },
+  render: function () {
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Weather App</li>
 
-    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-    <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</IndexLink>
-    <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</IndexLink>
-  </div>
-  )
-};
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+          </li>
+          <li>
+            <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</IndexLink>
+          </li>
+          <li>
+            <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</IndexLink>
+          </li>
+        </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+            <li>
+              <input type="search" placeholder="Search Weather"/>
+            </li>
+            <li>
+              <input type="submit" className="button"  value="Get Weather"/>
+            </li>
+
+            </ul>
+          </form>
+        </div>
+
+      </div>
+    );
+  }
+});
 
 module.exports = Nav;
