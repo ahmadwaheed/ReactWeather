dab01ba183609039eba93e333974f2d81f143530
Ahmad Waheed <ammwaheed@ucdavis.edu>
2017-09-03 19:03:13 -0700

    Styling Main and Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,18 +1,19 @@
 var React = require('react');
-
-//var Examples = React.createClass({
-  //render: function(){
-    //return(
-    //  <h3>Examples Component</h3>
-    //);
-//  }
-//});
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Few example locations</p>
+      <ol>
+        <li>
+          <Link to='/?location=Woodland'>Woodland, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Gujranwala'>Gujranwala, Punjab</Link>
+        </li>
+      </ol>
   </div>
   )
 };
